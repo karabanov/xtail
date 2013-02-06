d1cdcffbf3dff01b1ac397caff909c0cceaa1c46
Alexandr Karabanov <zend.karabanov@gmail.com>
2013-02-06 11:54:48 +0400

    Окончательный вариант

--- xtail_color.sh
+++ xtail_color.sh
@@ -6,7 +6,7 @@ do
   echo $line | sed "/pskov-nas/!d;
   s/accept/`echo -e \\\033[032maccept\\\033[0m`/;
   s/reject/`echo -e \\\033[031mreject\\\033[0m`/;
-  s/[ ]testvpn/`echo -e \\\033[035mtestvpn\\\033[0m`/"
+  s/testvpn/`echo -e \\\033[035mtestvpn\\\033[0m`/"
 
 done
 
