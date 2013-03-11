1037405e01042da232361491a8614a345eb590d8
Alexandr Karabanov <zend.karabanov@gmail.com>
2013-03-11 23:42:26 +0400

    wor in progres

--- xtail_color.sh
+++ xtail_color.sh
@@ -3,4 +3,5 @@
 /usr/bin/tail -F radius.log | sed -r "/pskov-nas/!d;
     s/accept/\x1b[32m&\x1b[0m/;
     s/reject/\x1b[31m&\x1b[0m/;
-    s/testvpn(.*) /\x1b[35m&\x1b[0m/;"
+    s/testvpn(.*) /\x1b[35m&\x1b[0m/;
+    s/[\t]//g;"
