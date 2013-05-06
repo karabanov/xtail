34318e8443a00550c1bc5d088c1d37644e7d01f2
Александр Карабанов <zend.karabanov@gmail.com>
2013-05-06 10:13:31 +0400

    Update xtail_color.sh

--- xtail_color.sh
+++ xtail_color.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-/usr/bin/tail -F radius.log | sed -r "/pskov-nas/!d;
+/usr/bin/tail -F ./log/radius-macs.log | sed -r "/pskov-nas/!d;
     s/accept/\x1b[32m&\x1b[0m/;
     s/reject/\x1b[31m&\x1b[0m/;
-    s/testvpn(.*) /\x1b[35m&\x1b[0m/;
-    s/[\t]//g;"
+    s/testvpn(.*)\s/\x1b[35m&\x1b[0m/;
+    s/[\t]/ /g;"
